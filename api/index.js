ce10f974c350ae45cf2a81271dd5bdde9c86bf2b
Benjaminlucky <42000766+Benjaminlucky@users.noreply.github.com>
2023-12-20 20:35:46 +0100

    create and save api route

--- api/index.js
+++ api/index.js
@@ -2,9 +2,13 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000 ');
 });
@@ -17,3 +21,4 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
